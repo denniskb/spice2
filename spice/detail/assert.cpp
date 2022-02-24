0de48cb968302e7dd13d139cf5299d904b3b34ae
dkb <dennis@bautembach.de>
2022-02-25 01:56:57 +0200

    Remove redundant using directive

--- spice/detail/assert.cpp
+++ spice/detail/assert.cpp
@@ -6,9 +6,7 @@
 
 namespace spice::detail {
 void assert_failed(char const* _file, int const line, char const* condition) {
-	using std::string;
-
-	const string file(_file);
+	std::string const file(_file);
 	std::stringstream error;
 	error << "Assertion failed (" << file.substr(file.find_last_of('/') + 1, file.length()) << ":"
 	      << line << "): " << condition;
