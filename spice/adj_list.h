0cbbc8bb38b809ef3b279caf3567678bc1d89556
dkb <dennis@bautembach.de>
2022-03-05 01:08:02 +0200

    Implement sentinel optimization
    Instead of comparing the unerlying pointers of adj_list::iterator's,
    iterators are agumented with a sentinel value representing the next
    source neuron's edge value. Compared are now rhs.edge vs lhs.sentinel.
    This saves one binary search for the end iterator at the price of
    reading an entire row's worth of edges, which will most probably be
    accessed anyway.

--- spice/adj_list.h
+++ spice/adj_list.h
@@ -19,11 +19,11 @@ public:
 	template <bool Const>
 	class iterator_t {
 	public:
-		using synapse_t = std::conditional_t<Const, Synapse const*, Synapse*>;
+		using synapse_t = std::conditional_t<Const, Synapse const, Synapse>;
 		struct edge {
 			Int src;
 			Int dst;
-			synapse_t syn;
+			synapse_t* syn;
 		};
 
 		using iterator_category = std::input_iterator_tag;
@@ -35,8 +35,8 @@ public:
 
 		edge operator*() const { return {Int(*_edge >> 32), Int(*_edge & 0xffffffff), _synapse}; }
 
-		bool operator==(const iterator_t& other) const { return _edge == other._edge; }
-		bool operator!=(const iterator_t& other) const { return _edge != other._edge; }
+		bool operator==(const iterator_t& other) const { return *_edge >= other._sentinel; }
+		bool operator!=(const iterator_t& other) const { return *_edge < other._sentinel; }
 
 		iterator_t& operator++() {
 			_edge++;
@@ -54,10 +54,12 @@ public:
 	private:
 		friend class adj_list;
 
-		UInt const* _edge  = nullptr;
-		synapse_t _synapse = nullptr;
+		UInt const* _edge   = nullptr;
+		synapse_t* _synapse = nullptr;
+		UInt _sentinel      = 0;
 
-		iterator_t(UInt const* e, synapse_t syn) : _edge(e), _synapse(syn){};
+		iterator_t(UInt const* e, synapse_t* syn, UInt sent) :
+		_edge(e), _synapse(syn), _sentinel(sent){};
 	};
 	using iterator       = iterator_t<false>;
 	using const_iterator = iterator_t<true>;
@@ -68,65 +70,52 @@ public:
 		SPICE_ASSERT(0 <= dst_count && dst_count < std::numeric_limits<Int32>::max());
 		SPICE_ASSERT(0 <= p && p <= 1);
 
-		if (p == 0)
-			return;
+		if (p > 0) {
+			_edges.reserve(src_count * dst_count * p);
 
-		_edges.reserve(src_count * dst_count * p);
+			if constexpr (!std::is_void_v<Synapse>)
+				_synapses.reserve(src_count * dst_count * p);
 
-		if constexpr (!std::is_void_v<Synapse>)
-			_synapses.reserve(src_count * dst_count * p);
+			util::xoroshiro64_128p rng(seed);
+			util::exponential_distribution<double> exprnd(1 / p - 1);
 
-		util::xoroshiro64_128p rng(seed);
-		util::exponential_distribution<double> exprnd(1 / p - 1);
+			for (UInt const src : util::range(src_count)) {
+				Int index    = 0;
+				double noise = 0;
+				for (;;) {
+					noise += exprnd(rng);
+					Int const dst = index++ + std::round(noise);
 
-		for (UInt const src : util::range(src_count)) {
-			Int index    = 0;
-			double noise = 0;
-			for (;;) {
-				noise += exprnd(rng);
-				Int const dst = index++ + std::round(noise);
+					if (__builtin_expect(dst >= dst_count, 0))
+						break;
 
-				if (__builtin_expect(dst >= dst_count, 0))
-					break;
+					_edges.push_back((src << 32) | dst);
 
-				_edges.push_back((src << 32) | dst);
-
-				if constexpr (!std::is_void_v<Synapse>)
-					_synapses.push_back({});
+					if constexpr (!std::is_void_v<Synapse>)
+						_synapses.push_back({});
+				}
 			}
 		}
-
 		// Add sentinel so as to avoid range checks
-		if (!_edges.empty())
-			_edges.push_back(std::numeric_limits<UInt>::max());
+		_edges.push_back(std::numeric_limits<UInt>::max());
 	}
 
-	Int size() const { return std::max(Int(0), static_cast<Int>(_edges.size()) - 1); }
+	Int size() const { return _edges.size() - 1; }
 
-	iterator begin() { return {_edges.data(), _synapses.data()}; }
-	const_iterator begin() const { return {_edges.data(), _synapses.data()}; }
-	const_iterator cbegin() const { return {_edges.data(), _synapses.data()}; }
-	iterator end() { return {_edges.data() + _edges.size(), _synapses.data() + _synapses.size()}; }
-	// TODO: clang-format line length
-	const_iterator end() const {
-		return {_edges.data() + _edges.size(), _synapses.data() + _synapses.size()};
-	}
-	const_iterator cend() const {
-		return {_edges.data() + _edges.size(), _synapses.data() + _synapses.size()};
-	}
+	iterator begin() { return {_edges.data(), _synapses.data(), _edges.front()}; }
+	const_iterator begin() const { return {_edges.data(), _synapses.data(), _edges.front()}; }
+	const_iterator cbegin() const { return begin(); }
+	iterator end() { return {nullptr, nullptr, std::numeric_limits<UInt>::max()}; }
+	const_iterator end() const { return {nullptr, nullptr, std::numeric_limits<UInt>::max()}; }
+	const_iterator cend() const { return end(); }
 
-	// TODO: Provide custom iterator that can iterate both IDs and synapses
-	// (will also be able to avoid 2nd lower_bound by checking for a changing src ID instead)
 	util::range_t<const_iterator> neighbors(Int const src) const {
 		SPICE_ASSERT(0 <= src && src < std::numeric_limits<Int32>::max());
 
-		UInt const first = src << 32;
-		UInt const last  = (src + 1) << 32;
-
-		return {std::next(begin(),
-		                  std::lower_bound(_edges.begin(), _edges.end(), first) - _edges.begin()),
-		        std::next(begin(),
-		                  std::lower_bound(_edges.begin(), _edges.end(), last) - _edges.begin())};
+		Int const index =
+		    (std::lower_bound(_edges.begin(), _edges.end(), src << 32) - _edges.begin());
+		return {{_edges.data() + index, _synapses.data() + index, 0},
+		        {nullptr, nullptr, (UInt(src) + 1) << 32}};
 	}
 
 private:
