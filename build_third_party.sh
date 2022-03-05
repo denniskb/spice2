a854c3538e62fd4979f10dc27d166d95c7259183
dkb <dennis@bautembach.de>
2022-03-05 05:51:45 +0200

    Improve third party build script
    - Disable unneccessary options
    - Enable parallel build

--- build_third_party.sh
+++ build_third_party.sh
@@ -8,12 +8,12 @@ BASE_FOLDER=$(pwd)
 cd "$BASE_FOLDER"
 mkdir -p third_party/googletest/build
 cd third_party/googletest/build
-cmake -DCMAKE_BUILD_TYPE=Release -Dgtest_disable_pthreads:BOOL=ON ..
-make
+cmake -DCMAKE_BUILD_TYPE=Release -DINSTALL_GTEST:BOOL=OFF -DBUILD_GMOCK:BOOL=OFF -Dgtest_disable_pthreads:BOOL=ON ..
+make -j 8
 
 # Build google benchmark
 cd "$BASE_FOLDER"
 mkdir -p third_party/benchmark/build
 cd third_party/benchmark/build
-cmake -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_ENABLE_TESTING:BOOL=OFF ..
-make
+cmake -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_ENABLE_INSTALL:BOOL=OFF -DBENCHMARK_INSTALL_DOCS:BOOL=OFF -DBENCHMARK_ENABLE_TESTING:BOOL=OFF -DBENCHMARK_ENABLE_GTEST_TESTS:BOOL=OFF -DBENCHMARK_ENABLE_WERROR:BOOL=OFF ..
+make -j 8
