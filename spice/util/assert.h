e632f3e1bcb343d8aaff7bdb94b1a811e9945c59
dkb <dennis@bautembach.de>
2022-03-06 23:37:52 +0200

    Add Perf build target
    Release with debug info but asserts turned off, for perf.

--- spice/util/assert.h
+++ spice/util/assert.h
@@ -1,8 +1,12 @@
 #pragma once
 
-#define SPICE_ASSERT(X)            \
-	if (__builtin_expect(!(X), 0)) \
-		::spice::util::assert_failed(__FILE__, __LINE__, #X);
+#ifdef SPICE_NO_ASSERT
+	#define SPICE_ASSERT(X)
+#else
+	#define SPICE_ASSERT(X)            \
+		if (__builtin_expect(!(X), 0)) \
+			::spice::util::assert_failed(__FILE__, __LINE__, #X);
+#endif
 
 namespace spice::util {
 void assert_failed(char const* file, int const line, char const* condition);
