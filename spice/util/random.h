47c6734dcce44011a233baf7f81ba5faf0cd4686
dkb <dennis@bautembach.de>
2022-03-11 05:23:25 +0200

    Implement independent random streams
    - Implement custom, fixed-size seed sequence based on Murmur3.
    - Implement ++ operator to obtain new seeds for subsequent iterations.
    - Implement stream(id) method to obtain independent streams within the
    same iteration.
    
    TODO: Implement rng hierarchy and pass rngs to methods.

--- spice/util/random.h
+++ spice/util/random.h
@@ -15,34 +15,164 @@
 namespace spice::util {
 namespace detail {
 template <std::integral Integer>
-inline Integer rotl(Integer const x, Int const k) {
+constexpr Integer rotl(Integer const x, Int const k) {
 	return (x << k) | (x >> ((sizeof(Integer) * 8) - k));
 }
+
+constexpr uint64_t fmix64(uint64_t k) {
+	k ^= k >> 33;
+	k *= 0xff51afd7ed558ccdllu;
+	k ^= k >> 33;
+	k *= 0xc4ceb9fe1a85ec53llu;
+	k ^= k >> 33;
+	return k;
+}
 }
 
-// Copy-able, fixed-size (128bit) seed_seq
-class seed_seq {
-public:
-	static constexpr Int SIZE = 4;
+struct seed_t {
+	UInt lo = 0;
+	UInt hi = 0;
+};
 
-	seed_seq(std::initializer_list<UInt> il) { std::seed_seq(il).generate(_seed, _seed + SIZE); }
+// based on splitmix64
+constexpr UInt hash(UInt x) {
+	x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9llu;
+	x = (x ^ (x >> 27)) * 0x94d049bb133111ebllu;
+	x = x ^ (x >> 31);
+	return x;
+}
 
-	template <class RandomIt>
-	void generate(RandomIt first, RandomIt last) {
-		Int i = 0;
-		while (first != last)
-			*first++ = _seed[i++ % SIZE];
+constexpr seed_t murmur(seed_t k) {
+	// non-zero seed (chosen arbitrarily)
+	seed_t h{0x2E4016967F18E81llu, 0x447567949F9AA86llu};
+
+	UInt const c1 = 0x87c37b91114253d5llu;
+	UInt const c2 = 0x4cf5ad432745937fllu;
+
+	k.lo *= c1;
+	k.lo = detail::rotl(k.lo, 31);
+	k.lo *= c2;
+	h.lo ^= k.lo;
+	h.lo = detail::rotl(h.lo, 27);
+	h.lo += h.hi;
+	h.lo = h.lo * 5 + 0x52dce729;
+	k.hi *= c2;
+	k.hi = detail::rotl(k.hi, 33);
+	k.hi *= c1;
+	h.hi ^= k.hi;
+	h.hi = detail::rotl(h.hi, 31);
+	h.hi += h.lo;
+	h.hi = h.hi * 5 + 0x38495ab5;
+	h.lo ^= 16;
+	h.hi ^= 16;
+	h.lo += h.hi;
+	h.hi += h.lo;
+	h.lo = detail::fmix64(h.lo);
+	h.hi = detail::fmix64(h.hi);
+	h.lo += h.hi;
+	h.hi += h.lo;
+
+	return h;
+}
+
+constexpr seed_t murmur(void const* ptr, UInt len) {
+	UInt8 const* const data = static_cast<UInt8 const*>(ptr);
+
+	// non-zero seed (chosen arbitrarily)
+	seed_t h{0x2E4016967F18E81llu, 0x447567949F9AA86llu};
+
+	UInt const c1 = 0x87c37b91114253d5llu;
+	UInt const c2 = 0x4cf5ad432745937fllu;
+
+	const Int nblocks  = len / 16;
+	UInt const* blocks = reinterpret_cast<UInt const*>(data);
+	for (Int i = 0; i < nblocks; i++) {
+		UInt k1 = blocks[i * 2 + 0];
+		UInt k2 = blocks[i * 2 + 1];
+
+		k1 *= c1;
+		k1 = detail::rotl(k1, 31);
+		k1 *= c2;
+		h.lo ^= k1;
+		h.lo = detail::rotl(h.lo, 27);
+		h.lo += h.hi;
+		h.lo = h.lo * 5 + 0x52dce729;
+		k2 *= c2;
+		k2 = detail::rotl(k2, 33);
+		k2 *= c1;
+		h.hi ^= k2;
+		h.hi = detail::rotl(h.hi, 31);
+		h.hi += h.lo;
+		h.hi = h.hi * 5 + 0x38495ab5;
 	}
 
-	Int size() const { return SIZE; }
+	const UInt8* tail = data + nblocks * 16;
 
-	template <class OutputIt>
-	void param(OutputIt it) {
-		std::copy(_seed, _seed + SIZE, it);
+	UInt k1 = 0;
+	UInt k2 = 0;
+
+	switch (len & 15) {
+		case 15: k2 ^= UInt(tail[14]) << 48;
+		case 14: k2 ^= UInt(tail[13]) << 40;
+		case 13: k2 ^= UInt(tail[12]) << 32;
+		case 12: k2 ^= UInt(tail[11]) << 24;
+		case 11: k2 ^= UInt(tail[10]) << 16;
+		case 10: k2 ^= UInt(tail[9]) << 8;
+		case 9:
+			k2 ^= UInt(tail[8]) << 0;
+			k2 *= c2;
+			k2 = detail::rotl(k2, 33);
+			k2 *= c1;
+			h.hi ^= k2;
+		case 8: k1 ^= UInt(tail[7]) << 56;
+		case 7: k1 ^= UInt(tail[6]) << 48;
+		case 6: k1 ^= UInt(tail[5]) << 40;
+		case 5: k1 ^= UInt(tail[4]) << 32;
+		case 4: k1 ^= UInt(tail[3]) << 24;
+		case 3: k1 ^= UInt(tail[2]) << 16;
+		case 2: k1 ^= UInt(tail[1]) << 8;
+		case 1:
+			k1 ^= UInt(tail[0]) << 0;
+			k1 *= c1;
+			k1 = detail::rotl(k1, 31);
+			k1 *= c2;
+			h.lo ^= k1;
+	};
+
+	h.lo ^= len;
+	h.hi ^= len;
+	h.lo += h.hi;
+	h.hi += h.lo;
+	h.lo = detail::fmix64(h.lo);
+	h.hi = detail::fmix64(h.hi);
+	h.lo += h.hi;
+	h.hi += h.lo;
+
+	return h;
+}
+
+// Copy-able, fixed-size seed_seq
+class seed_seq {
+public:
+	constexpr seed_seq(std::initializer_list<UInt32> il) : _seed(murmur(il.begin(), 4 * il.size())) {}
+	constexpr seed_t const& seed() const { return _seed; }
+
+	constexpr seed_seq operator++(int) {
+		auto result = *this;
+		_seed       = murmur(murmur(_seed));
+		return result;
+	}
+
+	constexpr seed_seq stream(UInt id) const {
+		seed_seq result = *this;
+		id              = hash(id);
+		result._seed.lo ^= id;
+		result._seed.hi ^= hash(id);
+		return result;
 	}
 
 private:
-	UInt32 _seed[SIZE];
+	seed_t _seed;
 };
 
 template <std::integral Integer, int Period, class Next>
@@ -50,10 +180,7 @@ class xoroshiro {
 public:
 	using result_type = Integer;
 
-	explicit xoroshiro(seed_seq sequence) {
-		auto* const ptr = reinterpret_cast<UInt32*>(&_state);
-		sequence.generate(ptr, ptr + Period / 32);
-	}
+	constexpr explicit xoroshiro(seed_seq const& seq) : _state(seq.seed()) {}
 
 	constexpr Integer min() { return 0; }
 	constexpr Integer max() { return std::numeric_limits<Integer>::max(); }
@@ -61,10 +188,12 @@ public:
 
 private:
 	struct state2 {
+		constexpr state2(seed_t const& s) : s0(s.lo), s1(s.hi) {}
 		Integer s0;
 		Integer s1;
 	};
 	struct state4 {
+		constexpr state4(seed_t const& s) : s0(s.lo), s1(s.lo >> 32), s2(s.hi), s3(s.hi >> 32) {}
 		Integer s0;
 		Integer s1;
 		Integer s2;
