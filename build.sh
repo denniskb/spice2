8208e921d0776817a66bdd922ce5ec1f553e3f23
dkb <dennis@bautembach.de>
2022-03-31 06:03:29 +0300

    Build release mode by default
    If no argument is passed to build.sh, build release mode by default.

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ "$1" = "release" ]; then
-	cd build/release
-	make -j 8
-elif [ "$1" = "debug" ]; then
+if [ "$1" = "debug" ]; then
 	cd build/debug
 	make -j 8
 elif [ "$1" = "perf" ]; then
 	cd build/perf
+	make -j 8
+else
+	cd build/release
 	make -j 8
 fi
