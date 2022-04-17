9b8fd29c8beb47b0c6dcc08e8f586348f33d8c2d
dkb <dennis@bautembach.de>
2022-04-17 15:11:15 +0300

    Fix configure.sh
    Fix configure.sh to reflect latest changes (dev build mode is now called
    "developer", spice_build_samples is now called spice_use_matplot and
    should not be enabled by default for anybody).

--- configure.sh
+++ configure.sh
@@ -5,20 +5,20 @@ BASE_FOLDER=$(pwd)
 cd "$BASE_FOLDER"
 mkdir -p build/release
 cd build/release
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE=-O2 -Dspice_build_samples:BOOL=TRUE ../..
-if [ "$1" = "dev" ]; then
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE=-O2 ../..
+if [ "$1" = "developer" ]; then
 	cmake -Dspice_build_benchmarks:BOOL=TRUE -Dspice_build_tests:BOOL=TRUE ../..
 fi
 
 cd "$BASE_FOLDER"
 mkdir -p build/debug
 cd build/debug
-cmake -DCMAKE_BUILD_TYPE=Debug -Dspice_build_samples:BOOL=TRUE ../..
-if [ "$1" = "dev" ]; then
+cmake -DCMAKE_BUILD_TYPE=Debug ../..
+if [ "$1" = "developer" ]; then
 	cmake -Dspice_build_benchmarks:BOOL=TRUE -Dspice_build_tests:BOOL=TRUE ../..
 fi
 
-if [ "$1" = "dev" ]; then
+if [ "$1" = "developer" ]; then
 	cd "$BASE_FOLDER"
 	mkdir -p build/perf
 	cd build/perf
