9aa4986b952606d8b276009b11ea5bf66757f72c
dkb <dennis@bautembach.de>
2022-04-22 23:58:11 +0300

    Comment sssp sample

--- samples/sssp.cpp
+++ samples/sssp.cpp
@@ -3,6 +3,17 @@
 using namespace spice;
 using namespace spice::util;
 
+/*
+This sample implements the single-source-shortest-pathS (plural, SSSP) algorithm on top of Spice.
+It illustrates Spice's generality and the simplicity of parallel programming when paired with
+local decision-making.
+It also illustrates per-population initialization of neurons.
+*/
+
+// For simplicity, we hard-code our graph as an adjacency matrix.
+// A more realistic scenario would be to read a graph description (such as GraphML) from file,
+// which is just as possible and just as easy to do.
+
 //      3 2 3
 //   1.---*---.3
 //  1/         \1
@@ -32,6 +43,12 @@ struct vertex {
 		bool fire              = false;
 	};
 
+	// This flavor of the init() method takes a span of all neurons inside the 'vertex'
+	// population. This is much more convenient than performing per-neuron initialization
+	// when the initial state is coming from a central location (such as a file) to begin with.
+	// Consider the alternative: Each of the (hundreds of thousands of) neurons would have to
+	// open the same file and seek to the neuron's offset inside the population. Not only would
+	// this be slow, it would also be combersome to write.
 	void init(std::span<neuron> neurons, auto&) {
 		neurons[src].distance = 0;
 		neurons[src].previous = &neurons[src];
@@ -46,6 +63,7 @@ struct vertex {
 };
 static_assert(CheckNeuron<vertex>());
 
+// In contrast to Brunel+, 'edge' is a stateful but NON-plastic synapse.
 struct edge {
 	struct synapse {
 		Int weight;
@@ -53,6 +71,10 @@ struct edge {
 
 	void init(synapse& syn, Int src, Int dst, auto&) const { syn.weight = adj_matrix[src][dst]; }
 
+	// SSSP's deliver() method is different from the deliver() method of all other models in that
+	// it accepts and additional 'src' parameter, giving it access to the firing neuron's state.
+	// Once again, the user simply defines a different 'flavor' (a different signature) of deliver()
+	// and Spice adapts the simulation accordingly (aka design by introspection).
 	void deliver(synapse const& syn, vertex::neuron const& src, vertex::neuron& dst) const {
 		if (src.distance + syn.weight < dst.distance) {
 			dst.distance = src.distance + syn.weight;
